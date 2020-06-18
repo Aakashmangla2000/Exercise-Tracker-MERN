dc829ffc34f4aeac455a976ec8b447f0151a30e1
Aakash Mangla <akkibaby90@yahoo.com>
2020-06-18 16:30:20 +0530

    Create user and exercises frontend done

---
+++ src/components/ExerciseList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ExerciseList extends Component {
+    render() {
+        return (
+            <div>
+                HI
+            </div>
+        )
+    }
+}
+
+export default ExerciseList
