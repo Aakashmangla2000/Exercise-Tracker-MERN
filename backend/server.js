4ecf315998119f300227711116da6229d4654ebb
Aakash Mangla <akkibaby90@yahoo.com>
2020-06-18 23:39:57 +0530

    App complete

--- backend/server.js
+++ backend/server.js
@@ -11,11 +11,17 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true  }
 );
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
+})
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.log('Unhandled Rejection at:', reason.stack || reason)
+  // Recommended: send the information to sentry.io
+  // or whatever crash reporting service you use
 })
 
 const exercisesRouter = require('./routes/exercises');
@@ -23,6 +29,8 @@ const usersRouter = require('./routes/users');
 
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
